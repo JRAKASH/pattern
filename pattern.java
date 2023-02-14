2525b19035b1992d36bf6c13132f52323ee2d010
JRAKASH <58143158+JRAKASH@users.noreply.github.com>
2023-02-14 08:22:17 +0530

    Create pattern.java
    pattern

---
+++ pattern.java
@@ -0,0 +1,27 @@
+package com.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Happy New Year");
+		for(int i=0;i<=4;i++){
+			for(int n=0;n<=i;n++) {
+		for(int j=0;j<1;j++) {
+			System.out.println("2");
+		}
+		for(int k=0;k<1;k++) {
+			System.out.println("0");
+		}
+		for(int l=0;l<1;l++) {
+			System.out.println("2");
+		}
+		for(int m=0;m<1;m++) {
+			System.out.println("3");
+		}
+		System.out.println("  ");
+	}
+		}
+System.out.println();
+}
+
+}
